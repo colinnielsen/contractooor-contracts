895d1b5006890abd65fcbc2d4795bc54eaa8c3f9
Colin Nielsen <colinrnielsen@gmail.com>
2023-04-24 09:31:35 -0600

    feat: add terminate by mutual consent

--- src/ContractooorAgreement.sol
+++ src/ContractooorAgreement.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 import {ISablier} from "@sablier/protocol/contracts/interfaces/ISablier.sol";
 import {Initializable} from "@openzeppelin/proxy/utils/Initializable.sol";
 import {IERC20} from "@openzeppelin/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {TerminationClauses, Agreement} from "contracts/lib/Types.sol";
 import {AgreementArbitrator} from "contracts/AgreementArbitrator.sol";
 
@@ -11,19 +12,58 @@ import {console2} from "forge-std/console2.sol";
 
 /// @title ContractooorAgreement
 /// @author @colinnielsen
-/// @notice Arbitrates agremements between Service Providers and Service Receivers
+/// @notice Arbitrates agremements between a service provider and a client
 contract ContractooorAgreement is Initializable {
+    using SafeERC20 for IERC20;
+
     error NOT_RECEIVER();
     error NOT_SENDER_OR_RECEIVER();
+    error NOT_CLIENT();
+    error NOT_SERVICE_PROVIDER();
+    error NOT_AUTHORIZED();
     error INCOMPATIBLE_TOKEN();
     error INVALID_END_TIME();
-
+    error STREAM_CANCELLATION_FAILED();
+˝˝
     AgreementArbitrator private arbitrator;
     ISablier private sablier;
+    uint96 public streamId;
     Agreement public agreement;
+
+    bytes32 public mutualConsentTerminationId;
 
     constructor() {
         _disableInitializers();
+    }
+
+    function onlyClient() internal view returns (address otherParty) {
+        Agreement memory _agreement = agreement;
+        if (msg.sender != _agreement.client) revert NOT_CLIENT();
+        otherParty = msg.sender == _agreement.client
+            ? _agreement.provider
+            : _agreement.client;
+    }
+
+    function onlyServiceProvider() internal view returns (address otherParty) {
+        Agreement memory _agreement = agreement;
+        if (msg.sender != _agreement.provider) revert NOT_SERVICE_PROVIDER();
+        otherParty = msg.sender == _agreement.client
+            ? _agreement.provider
+            : _agreement.client;
+    }
+
+    function onlyClientOrServiceProvider()
+        internal
+        view
+        returns (address otherParty)
+    {
+        Agreement memory _agreement = agreement;
+        if (
+            msg.sender != _agreement.client && msg.sender != _agreement.provider
+        ) revert NOT_SENDER_OR_RECEIVER();
+        otherParty = msg.sender == _agreement.client
+            ? _agreement.provider
+            : _agreement.client;
     }
 
     /// @notice initializes this agreement and begins a stream with the current tokens of this contract
@@ -53,20 +93,51 @@ contract ContractooorAgreement is Initializable {
         uint256 tokensToStream,
         uint256 termLength,
         Agreement calldata _agreement
-    ) public initializer returns (uint256 streamId) {
+    ) public initializer returns (uint256 _streamId) {
         arbitrator = AgreementArbitrator(msg.sender);
         sablier = _sablier;
         agreement = _agreement;
 
         IERC20(streamToken).approve(address(_sablier), tokensToStream);
 
-        streamId = sablier.createStream({
+        _streamId = sablier.createStream({
             recipient: _agreement.provider,
             deposit: tokensToStream,
             tokenAddress: address(streamToken),
             startTime: block.timestamp,
             stopTime: block.timestamp + termLength
         });
+
+        streamId = uint96(_streamId);
+    }
+
+    function terminateByMutualConsent(string memory ipfsHash) public {
+        address otherParty = onlyClientOrServiceProvider();
+        bytes32 mutualConsentCancellationHash = keccak256(
+            abi.encode(ipfsHash, otherParty)
+        );
+        if (mutualConsentCancellationHash == mutualConsentTerminationId) {
+            _terminateAgreement();
+        } else {
+            mutualConsentTerminationId = keccak256(
+                abi.encode(ipfsHash, msg.sender)
+            );
+        }
+    }
+
+    // cancel the stream and transfer unspent tokens back to the client
+    //   (sablier handles the refund of the service provider, who is the recipient)
+    function _terminateAgreement() internal {
+        (, , , address streamToken, , , , ) = sablier.getStream(streamId);
+
+        bool cancelled = sablier.cancelStream(streamId);
+        if (!cancelled) revert STREAM_CANCELLATION_FAILED();
+
+        IERC20(streamToken).safeTransferFrom(
+            address(this),
+            agreement.client,
+            IERC20(streamToken).balanceOf(address(this))
+        );
     }
 }
 
